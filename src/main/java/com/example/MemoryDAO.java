7216da75c4b1f91932a7310b31836c6e751e552f
sosuisen <hidekazu.kubota@gmail.com>
2022-12-09 10:19:30 +0900

    fix: use ArrayList<ToDo> instead of ToDoList

--- src/main/java/com/example/MemoryDAO.java
+++ src/main/java/com/example/MemoryDAO.java
@@ -1,37 +1,40 @@
 package com.example;
 
-import java.io.Serializable;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Optional;
 
 import com.google.gson.Gson;
 
-public class MemoryDAO implements DAO, Serializable {
-	private ToDoList todos = new ToDoList() {
+public class MemoryDAO implements DAO {
+	private ArrayList<ToDo> todos = new ArrayList<ToDo>() {
 		// Use instance initializer of anonymouse class
 		{
-			getList().add(new ToDo(1, "Design", LocalDate.of(2022, 12, 1), true));
-			getList().add(new ToDo(2, "Implementation", LocalDate.of(2022, 12, 7), false));
+			add(new ToDo(1, "Design", "2022-12-01", true));
+			add(new ToDo(2, "Implementation", "2022-12-07", false));
 		}
 	};
+	// If you use MemoryDAO's instance initializer,
+	// you need to write "storage." before "add" each time, which is a bit long.
+	//	{
+	//		storage.add(new ToDo(1, "", LocalDate.of(2022, 12, 1), false));
+	//	}
 
 	@Override
 	public Optional<ToDo> get(int id) {
-		Optional<ToDo> targetTodo = todos.getList().stream().filter(todo -> todo.getId() == id).findFirst();
+		Optional<ToDo> targetTodo = todos.stream().filter(todo -> todo.getId() == id).findFirst();
 		return targetTodo;
 	}
 
 	@Override
 	public ArrayList<ToDo> getAll() {
-		return todos.getList();
+		return todos;
 	}
 
 	@Override
-	public ToDo create(String title, LocalDate date) {
-		int newId = todos.getList().stream().max((todo1, todo2)->todo1.getId() - todo2.getId()).get().getId() + 1;
+	public ToDo create(String title, String date) {
+		int newId = todos.stream().max((todo1, todo2)->todo1.getId() - todo2.getId()).get().getId() + 1;
 		var newToDo = new ToDo(newId, title, date, false);
-		todos.getList().add(newToDo);
+		todos.add(newToDo);
 		
 		// For checking current todos 
 		System.out.println(todos);
@@ -41,20 +44,18 @@ public class MemoryDAO implements DAO, Serializable {
 
 	@Override
 	public Optional<ToDo> updateTitle(int id, String title) {
-		Optional<ToDo> targetTodo = todos.getList().stream().filter(todo -> todo.getId() == id).findFirst();
+		Optional<ToDo> targetTodo = todos.stream().filter(todo -> todo.getId() == id).findFirst();
 		if(targetTodo.isPresent()) targetTodo.get().setTitle(title);
 		
 		// For checking current todos 
 		System.out.println(todos);
 
-		serialize();
-		
 		return targetTodo;
 	}
 
 	@Override
-	public Optional<ToDo> updateDate(int id, LocalDate date) {
-		Optional<ToDo> targetTodo = todos.getList().stream().filter(todo -> todo.getId() == id).findFirst();
+	public Optional<ToDo> updateDate(int id, String date) {
+		Optional<ToDo> targetTodo = todos.stream().filter(todo -> todo.getId() == id).findFirst();
 		if(targetTodo.isPresent()) targetTodo.get().setDate(date);
 				
 		// For checking current todos 
@@ -65,7 +66,7 @@ public class MemoryDAO implements DAO, Serializable {
 
 	@Override
 	public Optional<ToDo> updateCompleted(int id, boolean completed) {
-		Optional<ToDo> targetTodo = todos.getList().stream().filter(todo -> todo.getId() == id).findFirst();		
+		Optional<ToDo> targetTodo = todos.stream().filter(todo -> todo.getId() == id).findFirst();		
 		if(targetTodo.isPresent()) targetTodo.get().setCompleted(completed);
 				
 		// For checking current todos 
@@ -76,14 +77,14 @@ public class MemoryDAO implements DAO, Serializable {
 
 	@Override
 	public Optional<Integer> delete(int id) {
-		boolean success = todos.getList().removeIf(todo->todo.getId() == id);
+		boolean success = todos.removeIf(todo->todo.getId() == id);
 				
 		// For checking current todos 
 		System.out.println(todos);
 	
 		return success ? Optional.of(id) : Optional.empty();
 	}
-	
+
 	public void serialize() {
 		var gson = new Gson();
 		var json = gson.toJson(todos);
